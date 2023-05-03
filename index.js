cdaa729e98449b5a2773029d707ae3c4d4d7a975
rtake <rtake1110@gmail.com>
2023-05-03 19:46:40 +0900

    support --name option

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const fs = require('fs');
 const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
 const package = JSON.parse(packageStr);
 
-console.log(package);
+const nameOption = process.argv.includes('--name');
+
+if(nameOption) {
+    console.log(package.name);
+} else {
+    console.log('optionがありません');
+}
