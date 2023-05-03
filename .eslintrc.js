4f7b290c8066d6c22eda6b78dae5afa11335819d
rtake <rtake1110@gmail.com>
2023-05-03 20:53:22 +0900

    Add ESLint setting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true
+    },
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    rules: {
+        quotes: ['error', 'single']
+    }
+};
