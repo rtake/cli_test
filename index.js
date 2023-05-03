4ebb56ceca6998659050d0fbcfe6f6378db9f85c
rtake <rtake1110@gmail.com>
2023-05-03 20:05:59 +0900

    feat: add yargs for command-line option parsing

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const fs = require('fs');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const { argv } = yargs(hideBin(process.argv));
+console.log(argv);
 
 const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
 const package = JSON.parse(packageStr);
 
-const nameOption = process.argv.includes('--name');
-
-if(nameOption) {
+if(argv.name) {
     console.log(package.name);
 } else {
     console.log('optionがありません');
