5bec832a98ea124a3a14398e3dd59bd18f5713d6
rtake <rtake1110@gmail.com>
2023-05-03 20:06:34 +0900

    feat: support command line options

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-const { argv } = yargs(hideBin(process.argv));
-console.log(argv);
+const { argv } = yargs(hideBin(process.argv))
+    .option('name', { describe: 'CLI名を表示' })
+    .option('file', { describe: 'Markdownファイルへのパス' });
 
 const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
 const package = JSON.parse(packageStr);
 
-if(argv.name) {
+if(argv.file) {
+    console.log(argv.file);
+} else if(argv.name) {
     console.log(package.name);
 } else {
     console.log('optionがありません');
