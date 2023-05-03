00d109f4764fa013c3843e913142295323d983dc
rtake <rtake1110@gmail.com>
2023-05-03 19:42:02 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const fs = require('fs');
+
+const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
+const package = JSON.parse(packageStr);
+
+console.log(package);
