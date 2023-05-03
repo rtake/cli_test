1f29c8dcf1390af8a12a9f6b11df2b9e26dd09d9
rtake <rtake1110@gmail.com>
2023-05-03 20:36:33 +0900

    feat: convert Markdown to HTML

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const path = require('path');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const { getPackageName } = require('./lib/name');
-const { readMarkdownFileSync } = require('./lib/file');
+const { readMarkdownFileSync, writeHtmlFileSync } = require('./lib/file');
+const { marked } = require('marked');
 
 const { argv } = yargs(hideBin(process.argv))
     .option('name', { describe: 'CLI名を表示' })
-    .option('file', { describe: 'Markdownファイルへのパス' });
+    .option('file', { describe: 'Markdownファイルへのパス' })
+    .option('out', {
+        describe: '出力するhtmlファイルの名前',
+        default: 'article.html'
+    });
 
 if(argv.name) {
     const name = getPackageName(); // fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8'});
     console.log(name);
+
+    // if --name specified, exit withut other processing
     process.exit(0);
 }
 
+// Convert .md -> .html
 const markdownStr = readMarkdownFileSync(path.resolve(__dirname, argv.file));
-console.log(markdownStr);
+const html = marked(markdownStr);
+writeHtmlFileSync(path.resolve(__dirname, argv.out), html);
